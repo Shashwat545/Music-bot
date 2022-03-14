0a549aea1c67bc9fa1dee411a51f6f287cee002b
Shashwat Singh <shashwatsingh545@gmail.com>
2022-03-14 06:20:32 +0530

    Added comments

--- index.js
+++ index.js
@@ -8,29 +8,25 @@ const ytdl = require("ytdl-core");
 //Now we create our client and login
 const client = new Discord.Client();
 client.login(token);
-
+//Now we will create a map to store the songs to be played
 const queue = new Map();
 //Next are some basic listeners
 client.once("ready", () => {
     console.log("Lessgo!");
 });
 client.once("reconnecting", () => {
-    console.log("Reconnecting!");
+    console.log("Reconnecting....");
 });
 client.once("disconnect", () => {
-    console.log("Disconnect!");
+    console.log("Disconnect....");
 });
-
+//Now we will create a function to read the messages that a user types
 client.on("message", async message => {
-
     if(message.author.bot) return;
-
     if(!message.content.startsWith(prefix)) return;
-
     const serverQueue = queue.get(message.guild.id);
-
     if(message.content.startsWith(`${prefix}play`)) {
-        execute(message, serverQueue);
+        start(message, serverQueue);
         return;
     }
     else if(message.content.startsWith(`${prefix}skip`)) {
@@ -45,10 +41,9 @@ client.on("message", async message => {
         message.channel.send("Oops! You entered an invalid command :(");
     }
 });
-
-async function execute(message, serverQueue) {
-    const args=message.content.split(" ");
-
+//Next function is for adding songs to queue
+async function start(message, serverQueue) {
+    const splits=message.content.split(" ");
     const voiceChannel=message.member.voice.channel;
     if(!voiceChannel)
         return message.channel.send("You haven't joined a voice channel yet :|");
@@ -56,13 +51,11 @@ async function execute(message, serverQueue) {
     if(!permissions.has("SPEAK")||!permissions.has("CONNECT")) {
         return message.channel.send("Give me the permissions to join and speak in the voice channel first :(");
     }
-
-    const songInfo=await ytdl.getInfo(args[1]);
+    const songInfo=await ytdl.getInfo(splits[1]);
     const song= {
         title:songInfo.videoDetails.title,
         url:songInfo.videoDetails.video_url,
     };
-
     if(!serverQueue) {
         const queueConstruct={
             textChannel:message.channel,
@@ -74,7 +67,6 @@ async function execute(message, serverQueue) {
         };
         queue.set(message.guild.id, queueConstruct);
         queueConstruct.songs.push(song);
-
         try {
             var connection=await voiceChannel.join();
             queueConstruct.connection=connection;
@@ -92,23 +84,24 @@ async function execute(message, serverQueue) {
         return message.channel.send(`${song.title} is added to the queue :)`);
     }
 }
-
+//Now the below function will be for skipping a song
 function skip(message, serverQueue) {
     if(!message.member.voice.channel)
         return message.channel.send("You need to be in a voice channel first :|");
     if(!serverQueue)
         return message.channel.send("You need to play something first :(");
-    serverQueue.connection.dispatcher.end();
+    serverQueue.connection.dispatcher.end(); //ends the current song
 }
+//Below function will be to stop playing the current song
 function stop(message,serverQueue) {
     if(!message.member.voice.channel)
         return message.channel.send("You need to be in a voice channel first :|");
     if(!serverQueue)
         return message.channel.send("You need to play something first :(");
-    serverQueue.songs=[];
-    serverQueue.connection.dispatcher.end();
+    serverQueue.songs=[];//empties the queue
+    serverQueue.connection.dispatcher.end();//And this ends the current song
 }
-
+//Now finally we create the function for playing songs
 function play(guild, song) {
     const serverQueue=queue.get(guild.id);
     if(!song) {
@@ -116,7 +109,6 @@ function play(guild, song) {
         queue.delete(guild.id);
         return;
     }
-
     const dispatcher=serverQueue.connection.play(ytdl(song.url)).on("finish", ()=> {
         serverQueue.songs.shift();
         play(guild, serverQueue.songs[0]);
@@ -124,5 +116,4 @@ function play(guild, song) {
     dispatcher.setVolumeLogarithmic(serverQueue.volume /5);
     serverQueue.textChannel.send(`Playing now: **${song.title}**`);
 }
-
 client.login(token);
